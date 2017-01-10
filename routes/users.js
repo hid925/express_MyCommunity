52ae7f4343438dfc1a27b099412a51317b772b15
Byoungyong <hid925@gmail.com>
2017-01-11 00:02:30 +0900

    working on an auth

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
+var db = require('../../config/db')();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+router.get('/login', function(req,res){
+  //로그인 처리
+  res.redirect('/');
+})
+
+
+router.post('/register', function(req,res){
+  var id = req.body.id;
+  var pw = req.body.pw;
+  var did = req.body.did;
+  var email = req.body.email;
+
+  //디비에 추가할자리
+
+  res.redirect('/');
 });
+
+router.get('/register', function(req, res){
+  res.render('users/register');
+});
+
 
 module.exports = router;
